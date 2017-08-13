8fbaf661dd4b9f213b83904405f85be15b192fe7
Wei Jietao <13415156317@163.com>
2017-08-13 23:45:31 +0800

    全局提交

---
+++ state2/README.md
@@ -0,0 +1,4 @@
+
+## 介绍 ##
+
+阶段一是构建一个可以正常启动的 webpack 框架，webpack.config.js 中仅配置 entry 和 out，更多详情参照链接：
---
+++ state2/webpack.config.js
@@ -0,0 +1,16 @@
+var path = require("path");
+
+// 路径常量
+var SRC_PATH = path.resolve(__dirname, "src");
+var DIST_PATH = path.resolve(__dirname, "dist");
+
+module.exports = {
+    entry: [
+        path.resolve(SRC_PATH, "entry.js")
+    ],
+    output: {
+        path: DIST_PATH,
+        publicPath: DIST_PATH,
+        filename: 'index.js'
+    }
+}
